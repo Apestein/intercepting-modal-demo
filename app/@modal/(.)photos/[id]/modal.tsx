10f40f0b9098f7645c1c1cee6e9e4bd40cae17b7
Apestein <ltn2057@protonmail.com>
2024-01-23 17:36:16 -0500

    save point

--- app/@modal/(.)photos/[id]/modal.tsx
+++ app/@modal/(.)photos/[id]/modal.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useEffect, useRef } from "react"
+import { buttonVariants } from "@/components/ui/button"
 
 export function Modal({ children }: { children: React.ReactNode }) {
   const router = useRouter()
@@ -22,6 +24,9 @@ export function Modal({ children }: { children: React.ReactNode }) {
       className="backdrop:bg-black/60 backdrop:backdrop-blur-sm text-3xl"
     >
       <div className="p-32">{children}</div>
+      <Link href="/catch-all" className={buttonVariants({ variant: "link" })}>
+        CatchAll Page
+      </Link>
     </dialog>
   )
 }
---
+++ app/catch-all/page.tsx
@@ -0,0 +1,7 @@
+export default function CatchAllPage() {
+  return (
+    <main className="bg-neutral-800 text-white min-h-screen text-3xl">
+      catch all page
+    </main>
+  )
+}
